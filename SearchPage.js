dc7b118870ec992f9db64f1ada597ad883121d7d
stanleycyang <stanley@stanleycyang.com>
2015-09-28 19:51:29 -0700

    Adding searching functionality

--- SearchPage.js
+++ SearchPage.js
@@ -63,8 +63,48 @@ let styles = StyleSheet.create({
   }
 });
 
+function urlForQueryAndPage(key, value, pageNumber) {
+  let data = {
+    country: 'uk',
+    pretty: '1',
+    encoding: 'json',
+    listing_type: 'buy',
+    action: 'search_listings',
+    page: pageNumber
+  };
+  data[key] = value;
+  let querystring = Object.keys(data)
+                          .map(key => key + '=' + encodeURIComponent(data[key]))
+                          .join('&');
+  return 'http://api.nestoria.co.uk/api?' + querystring;
+}
+
 class SearchPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchString: 'london',
+      isLoading: false,
+      message: ''
+    }
+  }
+  // underscore denotes private method
+  _executeQuery(query) {
+    console.log(query);
+    this.setState({
+      isLoading: true
+    });
+  }
+  onSearchPressed() {
+    let query = urlForQueryAndPage('place_name', this.state.searchString, 1);
+    this._executeQuery(query);
+  }
   render() {
+    let spinner = this.state.isLoading ?
+      ( <ActivityIndicatorIOS
+          hidden='true'
+          size='large' />) :
+      ( <View /> );
     return (
       <View style={styles.container}>
         <Text style={styles.description}>
@@ -75,18 +115,30 @@ class SearchPage extends Component {
         </Text>
         <View style={styles.flowRight}>
           <TextInput style={styles.searchInput}
+            value={this.state.searchString}
+            onChange={this.onSearchTextChanged.bind(this)}
             placeholder='Search via name or postcode' />
           <TouchableHighlight style={styles.button}>
             <Text style={styles.buttonText}>Go</Text>
           </TouchableHighlight>
         </View>
         <TouchableHighlight style={styles.button}
+          onPress={this.onSearchPressed.bind(this)}
           underlayColor='#99d9f4'>
           <Text style={styles.buttonText}>Location</Text>
         </TouchableHighlight>
         <Image source={require('image!house')} style={styles.image} />
+        {spinner}
+        <Text style={styles.description}>{this.state.message}</Text>
       </View>
     );
+  }
+  onSearchTextChanged(event) {
+    console.log('onSearchTextChanged');
+    this.setState({
+      searchString: event.nativeEvent.text
+    });
+    console.log(this.state.searchString);
   }
 }
 
